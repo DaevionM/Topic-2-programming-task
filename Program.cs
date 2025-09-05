6f571cab4b4b1736882661e431ea2de53a9ed032
Daevion Maitland <362726697@GoTVDSB.ca>
2025-09-05 09:39:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Topic_2_programming_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("money earned $" + (21 * 8));
+            Console.WriteLine(" 1 egg cost $" + (6.0 / 12));
+
+        }
+    }
+}
