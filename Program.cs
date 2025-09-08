2dfbe9c4adbef5b877c0846ad7db0293b6154f16
Daevion Maitland <362726697@GoTVDSB.ca>
2025-09-08 08:55:02 -0400

    done

--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("money earned $" + (21 * 8));
-            Console.WriteLine(" 1 egg cost $" + (6.0 / 12));
-
+            Console.WriteLine("Task 1");
+            Console.WriteLine("money earned" + (21 * 8).ToString("C"));
+            Console.WriteLine("Task 2");
+            Console.WriteLine("1 egg cost" + (6.0 / 12).ToString("C"));
+            Console.WriteLine("Task 3");
+            Console.WriteLine("The tax on 79.99 is" + (79.99 * .13).ToString("C"));
+            Console.WriteLine("Challenge");
+            Console.WriteLine("There are" + (5*12+6) * 2.54 + "centimeters in 5'6\""); 
         }
     }
 }
